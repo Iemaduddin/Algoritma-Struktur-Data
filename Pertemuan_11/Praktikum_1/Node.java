5521047c0dbb974149269bfaf4b726cda09ca52f
Iemaduddin <93432453+Iemaduddin@users.noreply.github.com>
2022-04-30 06:24:24 +0700

    Menambahkan Tugas Pertemuan 11

---
+++ Pertemuan_11/Praktikum_1/Node.java
@@ -0,0 +1,102 @@
+package Pertemuan_11.Praktikum_1;
+
+public class Node{
+    int data;
+    Node next;
+
+    public Node (int data, Node berikutnya){
+        this.data = data;
+        this.next = berikutnya;
+    }
+}
+
+class SingleLinkedList{
+    Node head;
+    Node tail;
+
+    public boolean IsEmpty(){
+        return head == null;
+    }
+
+    public void print(){
+        if(!IsEmpty()){
+            Node tmp =head;
+            System.out.print("Isi Linked List:\t");
+            while (tmp != null){
+                System.out.print(tmp.data + "\t");
+                tmp = tmp.next;
+            }
+            System.out.println("");
+        }else{
+            System.out.println("Linked list kosong");
+        }
+    }
+
+    public void addFirst(int input){
+        Node ndInput = new Node(input, null);
+        if(IsEmpty()){
+            head = ndInput;
+            tail = ndInput;
+        }else{
+            ndInput.next = head;
+            head = ndInput;
+        }
+    }
+
+    public void addLast(int input){
+        Node ndInput = new Node(input, null);
+        if(IsEmpty()){
+            head = ndInput;
+            tail = ndInput;
+        }else{
+            tail.next = ndInput;
+            tail = ndInput;
+        }
+    }
+
+    public void insertAfter(int key, int input){
+        Node ndInput = new Node(input, null);
+        Node temp = head;
+        do{
+            if(temp.data == key){
+                ndInput.next = temp.next;
+                temp.next = ndInput;
+                if(ndInput.next == null) tail = ndInput;
+                break;
+            }
+            temp = temp.next;
+        }while (temp != null);
+    }
+
+    public void insertAt(int index, int input){
+        if(index < 0){
+            System.out.println("indeks salah");
+        }else if(index == 0){
+            addFirst(input);
+        }else{
+            Node temp = head;
+            for(int i=0; i<index-1; i++){
+                temp = temp.next;
+            }
+            temp.next = new Node(input, temp.next);
+            if(temp.next.next == null) tail = temp.next;
+        }
+    }
+}
+
+class SLLMain{
+    public static void main(String[] args) {
+        SingleLinkedList singLL = new SingleLinkedList();
+        singLL.print();
+        singLL.addFirst(890);
+        singLL.print();
+        singLL.addLast(760);
+        singLL.print();
+        singLL.addFirst(700);
+        singLL.print();
+        singLL.insertAfter(700, 999);
+        singLL.print();
+        singLL.insertAt(3, 833);
+        singLL.print();
+    }
+}
